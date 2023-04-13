f3e46b65831cda0958d3b96b90cf6b1921333434
Vlad <vlad.kapitsa.mail.ru@gmail.com>
2023-04-13 18:15:09 +0300

    Update codegen_key.py

--- codegen_key.py
+++ codegen_key.py
@@ -3,17 +3,15 @@ import pyperclip
 import time
 import PIL.Image
 import pystray
-from threading import Thread, Lock
+from threading import Thread
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-checkpoint = "Salesforce/codegen-350M-mono"
+checkpoint = "Salesforce/codegen-350M-multi"
 
 model = AutoModelForCausalLM.from_pretrained(checkpoint)
 
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-
-lock = Lock()
 
 def ctrl_key(key):
     keyboard.press(29)
@@ -24,6 +22,46 @@ def ctrl_key(key):
     keyboard.release(key)
     time.sleep(0.02)
 
+def on_click(icon, item):
+    if str(item) == 'Назначить горячие клавиши':
+        print('Ты чё тут делаешь?')
+    elif str(item) == 'Выход':
+        icon.stop()
+
+tray_main = PIL.Image.open('eblo.png') # Program logo is under construction
+tray_progress = PIL.Image.open('logo.png')
+
+icon = pystray.Icon('AIcGen', tray_main, menu=pystray.Menu(
+        pystray.MenuItem('Назначить горячие клавиши', on_click),
+        pystray.MenuItem('Выход', on_click)
+)) # Program title is under construction
+
+def tray_init():
+    icon.run()
+
+def generate_code():
+    continue_exe = True
+    while continue_exe:
+        if keyboard.is_pressed(65):
+            icon.icon = tray_progress
+            ctrl_key(46)
+            text = pyperclip.paste()
+            completion = model.generate(**tokenizer(text, return_tensors="pt"), max_new_tokens=50)              
+            pyperclip.copy(tokenizer.decode(completion[0]))
+            ctrl_key(47)
+            icon.icon = tray_main
+
+        if not thread_tray.is_alive():
+            continue_exe = False
+
+if __name__ == '__main__':
+    thread_generator = Thread(target=generate_code)
+    thread_tray = Thread(target=tray_init)
+
+    thread_generator.start()
+    thread_tray.start()
+
+# Examples
 def fibonacci(n):
     """returns nth number of Fibonacci sequence"""
 
@@ -36,39 +74,4 @@ def sum_of_elements_of_L(L):
 def RecursiveFactorial(n):
     pass
 
-image = PIL.Image.open('eblo.png') # Program logo is under construction
-
-def on_click(icon, item):
-    if str(item) == 'Назначить горячие клавиши':
-        print('Ты чё тут делаешь?')
-    elif str(item) == 'Выход':
-        icon.stop()
-
-def tray_init():
-    icon = pystray.Icon('AIcGen', image, menu=pystray.Menu(
-            pystray.MenuItem('Назначить горячие клавиши', on_click),
-            pystray.MenuItem('Выход', on_click)
-    )) # Program title is under construction
-
-    icon.run()
-
-def generate_code():
-    continue_exe = True
-    while continue_exe:
-        if keyboard.is_pressed(65):
-            ctrl_key(46)
-            text = pyperclip.paste()
-            completion = model.generate(**tokenizer(text, return_tensors="pt"), max_new_tokens=50)              
-            pyperclip.copy(tokenizer.decode(completion[0]))
-            ctrl_key(47)
-
-        if not thread_tray.is_alive():
-            continue_exe = False
-
-if __name__ == '__main__':
-    
-    thread_generator = Thread(target=generate_code)
-    thread_tray = Thread(target=tray_init)
-
-    thread_generator.start()
-    thread_tray.start()
+# void greetUser(string username) {
