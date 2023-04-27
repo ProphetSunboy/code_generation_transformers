0c4f86ebdc78ba6c283577f7affa5b154dabfb88
Vlad <vlad.kapitsa.mail.ru@gmail.com>
2023-04-27 11:19:38 +0300

    Update dekstop.py

--- dekstop.py
+++ dekstop.py
@@ -9,17 +9,40 @@ import pandas as pd
 
 from codegen import generate_code
 
+def replace_datatypes(str):
+    data_types = ['string', 'int', 'bool', 'char', 'float', 'double']
+    return ', '.join([word for word in str.split() if word.lower() not in data_types])
+
+def parse_input(text):
+
+    function_name, params = text.split(':')[0].split('{')[0].split('(')
+    function_name = function_name.split(' ')[1]
+    params = params.split(')')[0]
+    formatted_params = replace_datatypes(params)
+
+    python = replace_datatypes(f'def {function_name}({formatted_params}):')
+    js = replace_datatypes(f'function {function_name}({formatted_params})' + '{')
+    c_2plus = f'void {function_name}({params})' + '{' 
+    c_lang = f'#include <stdio.h>\nvoid {function_name}({params})' + '{'
+    go_lang = f'func {function_name}({params})' + '{'
+    java = f'public void {function_name}({params})' + '{'
+
+    signatures = {'Py': python, 'JS': js, 'C++': c_2plus,
+                  'C': c_lang, 'Go': go_lang, 'Java': java}
+    
+    return signatures
+
 def generate():
     correct_inputs = True
     try:
         max_tokens = int(number_of_tokens_entry.get().strip())
-        temperature = float(temperature_entry.get().strip())
+        temperature = round(float(temperature_entry.get().strip()), 2)
     except ValueError:
         messagebox.showinfo('Некорректные данные',
             'Количество токенов и температура генерации должны быть числовыми')
         correct_inputs = False
 
-    input_seq = input_txt.get().strip()
+    input_seq = input_txt.get('1.0', END).strip()
     if len(input_seq) == 0:
         messagebox.showinfo('Пустой ввод',
                             'Пожалуйста введите сигнатуру функции')
@@ -27,77 +50,31 @@ def generate():
 
     if correct_inputs:
         output_txt.delete('1.0', END)
-        splitted_input = input_seq.split(' ', maxsplit=1)
+        signatures = parse_input(input_seq)
 
         if python.get() == 1:
             output_txt.insert(END, '\n\nPython:\n')
-            generate_code(,max_tokens, temperature)
+            output_txt.insert(END, generate_code(signatures.get('Py'), max_tokens, temperature))
 
         if js.get() == 1:
-            output_txt.insert(END, '\n\JavaScript:\n')
-            generate_code(,max_tokens, temperature)
+            output_txt.insert(END, '\n\nJavaScript:\n')
+            output_txt.insert(END, generate_code(signatures.get('JS'), max_tokens, temperature))
 
         if c_2plus.get() == 1:
-            output_txt.insert(END, '\n\C++:\n')
-            generate_code(,max_tokens, temperature)
+            output_txt.insert(END, '\n\nC++:\n')
+            output_txt.insert(END, generate_code(signatures.get('C++'), max_tokens, temperature))
 
         if c_lang.get() == 1:
-            output_txt.insert(END, '\n\C:\n')
-            generate_code(,max_tokens, temperature)
+            output_txt.insert(END, '\n\nC:\n')
+            output_txt.insert(generate_code(signatures.get('C'), max_tokens, temperature))
 
         if go_lang.get() == 1:
-            output_txt.insert(END, '\n\Go:\n')
-            generate_code(,max_tokens, temperature)
+            output_txt.insert(END, '\n\nGo:\n')
+            output_txt.insert(generate_code(signatures.get('Go'), max_tokens, temperature))
 
         if java.get() == 1:
-            output_txt.insert(END, '\n\Java:\n')
-            generate_code(,max_tokens, temperature)
-
-        generate_code(input_seq, max_tokens, temperature)
-    pass
-    # predictions_tf.delete(1.0, END)
-    # sentences = sentences_tf.get(1.0, END).split('\n')
-
-    # sentences_correctness = []
-    # for i in range(len(sentences) - 1):
-    #     iscorrect = bool(sentences[i].strip())
-    #     sentences_correctness.append(iscorrect)
-    #     if not iscorrect:
-    #         messagebox.showinfo('Некорректное предложение',
-    #             f'Предложение {i+1} введено некорректно')
-
-    # if not all(sentences_correctness):
-    #     messagebox.showinfo('Ошибка анализа',
-    #         'Введите корректный текст для анализа')
-    # else:
-    #     if model_type.get() == 0:
-    #         predictions = model_ru.predict(np.array(sentences[:-1]))
-    #     else:
-    #         predictions = model.predict(np.array(sentences[:-1]))
-
-    #     if output_type.get() == 0:
-    #         for x in range(0, len(predictions)):
-    #             predictions_tf.insert(float(x+1), str(predictions[x]) + '\n') 
-
-    #         messagebox.showinfo('Среднее',
-    #             str(sum(predictions) / len(predictions)))
-
-    #     else:
-    #         for x in range(0, len(predictions)):
-    #             if predictions[x] < -0.1:
-    #                 predictions_tf.insert(float(x+1), 'Негативный' + '\n')
-    #             elif predictions[x] > 0.1:
-    #                 predictions_tf.insert(float(x+1), 'Положительный' + '\n')
-    #             else:
-    #                 predictions_tf.insert(float(x+1), 'Нейтральный' + '\n')
-    #         pred_avg = sum(predictions) / len(predictions)
-    #         if pred_avg < -0.1:
-    #             messagebox.showinfo('Среднее', 'Негативно')
-    #         elif pred_avg > 0.1:
-    #             messagebox.showinfo('Среднее', 'Положительно')
-    #         else:
-    #             messagebox.showinfo('Среднее', 'Нейтрально')
-
+            output_txt.insert(END, '\n\nJava:\n')
+            output_txt.insert(generate_code(signatures.get('Java'), max_tokens, temperature))
 
 def show_help():
     messagebox.showinfo('Помощь', 'Язык анализа следует выбирать исходя '
@@ -217,7 +194,7 @@ generate_button = Button(
     text='Сгенерировать код',
     width=20,
     height=2,
-    command=analyze_sentences,
+    command=generate,
 )
 generate_button.pack(anchor=S, padx=(0, 0), pady=(10, 0))
 
