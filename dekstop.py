41b648c11b077d02ca931e3e33f04f9d4ecdb60e
Vlad <vlad.kapitsa.mail.ru@gmail.com>
2023-05-02 18:27:05 +0300

    Update dekstop.py

--- dekstop.py
+++ dekstop.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
+import customtkinter as ctk
+from PIL import Image
 
 from codegen import generate_code
 
@@ -9,14 +11,14 @@ def remove_datatypes(str):
     remove_datatypes(str)
         Return str without datatypes.
     '''
-    data_types = ['string', 'int', 'bool', 'char', 'float', 'double']
+    data_types = ['string', 'int', 'bool', 'char', 'float', 'double', 'integer']
     return ', '.join([word for word in ''.join(str.split(',')).split() if word.lower() not in data_types])
 
 def split_outputs():
     '''
     Function to separate outputs.
     '''
-    return '\n\n_______________________________\n\n'
+    return f'\n\n{"_"*90}\n\n'
 
 def format_f_name(f_name, lang='not py'):
     '''
@@ -46,6 +48,7 @@ def parse_input(text):
             }
     '''
     function_name, params = text.split(':')[0].split('{')[0].split('(')
+    #comments = text.split(':')[1].strip() or text.split('{')[1].strip() # Add comments to the end of the languages
     function_name = function_name.split(' ')[-1]
     params = params.split(')')[0]
     formatted_params = remove_datatypes(params)
@@ -75,7 +78,7 @@ def generate():
         temperature_inp = round(float(temperature_entry.get().strip()), 2)
     except ValueError:
         messagebox.showwarning('Некорректные данные',
-            'Количество токенов и температура генерации должны\nбыть числовыми')
+            'Значения полей "Количество токенов" и "Температура генерации"\n должны быть числовыми')
         correct_inputs = False
 
     input_seq = input_txt.get('1.0', END).strip()
@@ -94,27 +97,27 @@ def generate():
         output_txt.delete('1.0', END)
         
         if python.get() == 1:
-            output_txt.insert(END, 'Python:\n')
+            output_txt.insert(END, 'Python:\n\n')
             output_txt.insert(END, generate_code(signatures.get('Py'), max_tokens=n_tokens, temperature=temperature_inp) + split_outputs())
 
         if js.get() == 1:
-            output_txt.insert(END, 'JavaScript:\n')
+            output_txt.insert(END, 'JavaScript:\n\n')
             output_txt.insert(END, generate_code(signatures.get('JS'), max_tokens=n_tokens, temperature=temperature_inp) + split_outputs())
 
         if c_2plus.get() == 1:
-            output_txt.insert(END, 'C++:\n')
+            output_txt.insert(END, 'C++:\n\n')
             output_txt.insert(END, generate_code(signatures.get('C++'), max_tokens=n_tokens, temperature=temperature_inp) + split_outputs())
 
         if c_lang.get() == 1:
-            output_txt.insert(END, 'C:\n')
+            output_txt.insert(END, 'C:\n\n')
             output_txt.insert(END, generate_code(signatures.get('C'), max_tokens=n_tokens, temperature=temperature_inp) + split_outputs())
 
         if go_lang.get() == 1:
-            output_txt.insert(END, 'Go:\n')
+            output_txt.insert(END, 'Go:\n\n')
             output_txt.insert(END, generate_code(signatures.get('Go'), max_tokens=n_tokens, temperature=temperature_inp) + split_outputs())
 
         if java.get() == 1:
-            output_txt.insert(END, 'Java:\n')
+            output_txt.insert(END, 'Java:\n\n')
             output_txt.insert(END, generate_code(signatures.get('Java'), max_tokens=n_tokens, temperature=temperature_inp) + split_outputs())
 
 def show_help():
@@ -123,7 +126,7 @@ def show_help():
     '''
     messagebox.showinfo('Помощь',
                 'Количество токенов это длина генерируемой\n'
-                'последовательности в символах\n'
+                'последовательности в символах (ограничене 999 символами)\n'
                 'Температура генерации это коэффициент случайности\n'
                 'генерации (меньшие значения соответствуют более\n'
                 'стабильной генерации).\n'
@@ -134,49 +137,44 @@ def show_help():
                 'на кнопку "Сгенерировать код".'
     )
 
-root = Tk()
-root.geometry('900x450')
+root = ctk.CTk()
+root_width, root_height = root.winfo_screenwidth(), root.winfo_screenheight()
+root.geometry(f'{root_width}x{root_height}')
 root.title("Генерация кода")
+root.state('zoomed')
+root.minsize(1350, 700)
 root.iconbitmap(default="icons/main_logo.ico")
 
-parameters = Frame(root)
-parameters.pack(expand=1, fill=X, padx=(0, 0), pady=(0, 20))
+parameters = ctk.CTkFrame(root)
+parameters.pack(fill=X, side=TOP)
 
-number_of_tokens_label = Label(
+number_of_tokens_label = ctk.CTkLabel(
     parameters,
-    text="Количество токенов:        ",
-    font= ('Arial', 11),
-    background='white',
-    highlightbackground="black",
-    highlightcolor='black',
-    highlightthickness=1,
+    text="Количество токенов: ",
+    font=('Roboto', 16),
 )
-number_of_tokens_label.pack(side=LEFT, anchor=NW, padx=(20, 1))
+number_of_tokens_label.pack(side=LEFT, anchor=NW, padx=(60, 0), pady=(10,10))
 
-number_of_tokens_entry = Entry(
+number_of_tokens_entry = ctk.CTkEntry(
     parameters,
-    font= ('Arial', 11),
-    width=3,
+    font=('Roboto', 16),
+    width=40,
 )
-number_of_tokens_entry.pack(side=LEFT,anchor=N, padx=0, pady=(1, 0))
+number_of_tokens_entry.pack(side=LEFT,anchor=N, padx=0, pady=(10,10))
 
-temperature_label = Label(
+temperature_label = ctk.CTkLabel(
     parameters,
     text="Температура генерации: ",
-    font= ('Arial', 11),
-    background='white',
-    highlightbackground="black",
-    highlightcolor='black',
-    highlightthickness=1,
+    font=('Roboto', 16),
 )
-temperature_label.pack(side=LEFT,anchor=N, padx=(10, 1))
+temperature_label.pack(side=LEFT,anchor=N, padx=(10, 0), pady=(10,10))
 
-temperature_entry = Entry(
+temperature_entry = ctk.CTkEntry(
     parameters,
-    font= ('Arial', 11),
-    width=4,
+    font=('Roboto', 16),
+    width=46,
 )
-temperature_entry.pack(side=LEFT, anchor=N, padx=0, pady=(1, 0))
+temperature_entry.pack(side=LEFT, anchor=N, padx=0, pady=(10,10))
 
 python = IntVar()
 js = IntVar()
@@ -185,77 +183,73 @@ c_lang = IntVar()
 go_lang = IntVar()
 java = IntVar()
 
-python_checkbutton = ttk.Checkbutton(parameters,text='Python', variable=python)
-python_checkbutton.pack(side=LEFT, anchor=N, padx=(80, 4))
+java_checkbutton = ctk.CTkCheckBox(parameters, text='Java', font=('Roboto', 16), variable=java)
+java_checkbutton.pack(side=RIGHT, anchor=N, padx=(0, 60), pady=(10,10))
 
-js_checkbutton = ttk.Checkbutton(parameters,text='JavaScript', variable=js)
-js_checkbutton.pack(side=LEFT, anchor=N, padx=4)
+go_lang_checkbutton = ctk.CTkCheckBox(parameters, text='Go', font=('Roboto', 16), variable=go_lang)
+go_lang_checkbutton.pack(side=RIGHT, anchor=N, padx=0, pady=(10,10))
 
-c_2plus_checkbutton = ttk.Checkbutton(parameters,text='C++', variable=c_2plus)
-c_2plus_checkbutton.pack(side=LEFT, anchor=N, padx=4)
+c_lang_checkbutton = ctk.CTkCheckBox(parameters, text='C', font=('Roboto', 16), variable=c_lang)
+c_lang_checkbutton.pack(side=RIGHT, anchor=N, padx=0, pady=(10,10))
 
-c_lang_checkbutton = ttk.Checkbutton(parameters,text='C', variable=c_lang)
-c_lang_checkbutton.pack(side=LEFT, anchor=N, padx=4)
+c_2plus_checkbutton = ctk.CTkCheckBox(parameters, text='C++', font=('Roboto', 16), variable=c_2plus)
+c_2plus_checkbutton.pack(side=RIGHT, anchor=N, padx=0, pady=(10,10))
 
-go_lang_checkbutton = ttk.Checkbutton(parameters,text='Go', variable=go_lang)
-go_lang_checkbutton.pack(side=LEFT, anchor=N, padx=4)
+js_checkbutton = ctk.CTkCheckBox(parameters, text='JavaScript', font=('Roboto', 16), variable=js)
+js_checkbutton.pack(side=RIGHT, anchor=N, padx=(0, 30), pady=(10,10))
 
-java_checkbutton = ttk.Checkbutton(parameters,text='Java', variable=java)
-java_checkbutton.pack(side=LEFT, anchor=N, padx=4)
+python_checkbutton = ctk.CTkCheckBox(parameters, text='Python', font=('Roboto', 16), variable=python)
+python_checkbutton.pack(side=RIGHT, anchor=N, padx=(0, 10), pady=(10,10))
 
-text_boxes = Frame(root)
-text_boxes.pack()
+text_boxes = ctk.CTkFrame(root)
+text_boxes.pack(anchor=CENTER, pady=(80,0))
 
-input_frame = Frame(text_boxes)
-output_frame = Frame(text_boxes)
+input_frame = ctk.CTkFrame(text_boxes)
+output_frame = ctk.CTkFrame(text_boxes)
 
 input_frame.pack(side=LEFT)
-output_frame.pack(side=LEFT)
+output_frame.pack(side=RIGHT, padx=(1, 0))
 
-input_label = Label(
+input_label = ctk.CTkLabel(
     input_frame,
     text="Введите функцию: ",
-    font= ('Arial', 11),
+    font=('Roboto', 18),
 )
-input_label.pack(anchor=NW, padx=(20, 0), pady=(0, 0))
+input_label.pack(anchor=NW, padx=(20, 0), pady=(10, 0))
 
-output_label = Label(
+output_label = ctk.CTkLabel(
     output_frame,
     text="Результат генерации: ",
-    font= ('Arial', 11),
+    font=('Roboto', 18),
 )
-output_label.pack(anchor=NE, padx=(0, 235), pady=(0, 0))
+output_label.pack(anchor=NW, padx=(16, 0), pady=(10, 0))
 
-input_txt = Text(input_frame, width=46, height=14, wrap=NONE)
-input_txt.pack(side=LEFT,anchor=NW, padx=(20, 0), pady=(0, 0))
+input_txt = ctk.CTkTextbox(input_frame, font=('Roboto', 14), width=640, height=400, wrap=NONE)
+input_txt.pack(side=LEFT,anchor=NW, padx=(20, 10), pady=(0, 20))
 
-output_txt = Text(output_frame, width=46, height=14, wrap=NONE)
-output_txt.pack(anchor=NE, padx=20, pady=(0, 0))
+output_txt = ctk.CTkTextbox(output_frame, font=('Roboto', 14), width=640, height=400, wrap=NONE)
+output_txt.pack(side=LEFT, anchor=NW, padx=(10, 20), pady=(0, 20))
 
-generate_button = Button(
+generate_button = ctk.CTkButton(
     root,
     text='Сгенерировать код',
-    width=20,
-    height=2,
+    font=('Roboto', 20),
+    width=120,
+    height=48,
+    corner_radius=45,
     command=generate,
 )
 generate_button.pack(anchor=S, padx=(0, 0), pady=(10, 0))
 
-help_button = Button(
+help_button = ctk.CTkButton(
     root, 
     text='Помощь',
-    width=8,
-    height=2,
-    command=show_help
+    font=('Roboto', 16),
+    width=32,
+    height=24,
+    corner_radius=45,
+    command=show_help,
 )
-help_button.pack(anchor=SE, padx=(0, 8), pady=(0, 8))
-
-scroll_input_y = Scrollbar(command=input_txt.yview)
-scroll_input_x = Scrollbar(command=input_txt.xview)
-input_txt.config(yscrollcommand=scroll_input_y.set, xscrollcommand=scroll_input_x)
-
-scroll_output_y = Scrollbar(command=input_txt.yview)
-scroll_output_x = Scrollbar(command=input_txt.xview)
-input_txt.config(yscrollcommand=scroll_output_y.set, xscrollcommand=scroll_output_x)
+help_button.pack(side=BOTTOM, anchor=SE, padx=(0, 10), pady=(0, 10))
 
 root.mainloop()
