91afa56449688345965bbd543dacd2a6887c394e
Vlad Kapitsa <vlad.kapitsa.mail.ru@gmail.com>
2023-06-11 17:21:44 +0300

    Create generation_prompts.py

---
+++ generation_prompts.py
@@ -0,0 +1,97 @@
+# Python
+
+def print_hello_world():
+    """Print 'Hello World!'."""
+
+def fibonacci(n):
+    """returns nth number of Fibonacci sequence"""
+
+def is_e_in_L(e, L):
+    """return True if e in list L"""
+
+def sum_of_elements_of_L(L):
+    pass
+
+def recursive_factorial(n):
+    pass
+
+def count_words(filename):
+    """Count the number of occurrences of each word in the file"""
+
+def load_pandas_dataframe(filepath):
+	"""Load csv file and return dataframe"""
+        
+def calculate_mean_of_column_pandas(df, col):
+    """Return mean value of pandas dataframe column using loc"""
+
+def initialize_zero_matrix(n, m):
+    """Return numpy zero-matrix with size (n, m)"""
+
+def initialize_random_matrix(n, m, seed):
+    """Return randomly initialized numpy matrix with size (n, m), seed=seed"""
+
+def numpy_convert_to_float_normalize(arr):
+	"""Convert arr to numpy array dtype='f' and divide by 255"""
+        
+def next_item(xs, item):
+    ''"""
+    Given a sequence of items xs and a specific item, return the index of item
+    immediately following the item specified.
+    If item not in xs return None.
+    """
+
+def to_hex(num) :
+    """
+    Given an integer num, return a string representing its
+    hexadecimal representation.
+    """
+
+def binomial_coefficients(n, r):
+    """
+    Return binomial coefficients using scipy.
+    """
+
+def generate_password(length):
+	"""Return randomly generated alphanumerical password with len length."""
+        
+def generate_password_hash(password):
+    """Return hash of password."""
+
+def validate_email(email, reg):
+	"""Validate email using regular expression reg"""
+        
+def validate_email(email):
+	"""Validate email using regular expression"""
+
+def normalize_image(img):
+	"""Normalize image values"""
+
+def preprocess_data(x, y):
+	"""Normalize data with std using Tensorflow."""
+        
+def split_data(x, y, split_size):
+	"""Split data into train and test datasets using TensorFlow."""
+
+def build_model():
+	"""Build ResXNet model using Keras"""
+
+def build_model():
+	"""Build TensorFlow image classification sequential model"""
+        
+def train_model(model, epochs, x, y, validation_split):
+	"""Train TensorFlow model during epochs with input features x and labels y"""
+        
+def predict_model(model, x_test):
+	"""Return model predictions on test data x_test using Tensorflow"""
+
+# JavaScript
+
+def toHex(num):
+
+# C++
+
+# C
+
+# Java
+
+# Go
